a40d8e2f3fa0c3f13c2cc0c8d0b0c036b5cdee3c
kken0201 <kken.com@gmail.com>
2020-02-11 17:51:52 +0900

    朝ならおはようございますとあいさつする実装

--- src/Greeter.spec.ts
+++ src/Greeter.spec.ts
@@ -3,14 +3,14 @@ import Clock from './Clock'
 
 test('朝ならおはようございます', () => {
     const getcurrentTime = Clock.prototype.getCurrentTime = jest.fn();
-    getcurrentTime.mockImplementationOnce(() => new Date('2000-01-01T08:00:00.000Z'))
+    getcurrentTime.mockImplementationOnce(() => new Date('2000-01-01T08:00:00.000'))
     const greeter = new Greeter(new Clock())
     expect(greeter.greet()).toBe('おはようございます')
 })
 
 test('朝でないなら挨拶しない', () => {
     const getcurrentTime = Clock.prototype.getCurrentTime = jest.fn();
-    getcurrentTime.mockImplementationOnce(() => new Date('2000-01-01T22:00:00.000Z'))
+    getcurrentTime.mockImplementationOnce(() => new Date('2000-01-01T22:00:00.000'))
     const greeter = new Greeter(new Clock())
     expect(greeter.greet()).toBe('')
 })
--- src/Greeter.ts
+++ src/Greeter.ts
@@ -1,13 +1,17 @@
 import Clock from './Clock'
+import { getHours } from 'date-fns'
 
 export default class Greeter {
     private clock: Clock
     constructor(clock: Clock) {
         this.clock = clock
     }
-    greet(currentTime: Date = new Date()) {
-        if (true) { // currentTimeが朝なら
+    greet() {
+        const currentTime = this.clock.getCurrentTime()
+        const currentHour = getHours(currentTime)
+        if (currentHour >= 5 && currentHour < 12) { // currentTimeが朝なら
             return 'おはようございます'
         }
+        return ''
     }
 }
