06b9be1d4139360ddf0ef58cfc3271ab791993fd
kken0201 <kken.com@gmail.com>
2020-02-11 18:08:06 +0900

    test: MorningTimeRangeをDIするように

--- src/Greeter.spec.ts
+++ src/Greeter.spec.ts
@@ -1,16 +1,17 @@
 import Greeter from './Greeter'
+import MorningTimeRange from './MorningTimeRange'
 import Clock from './Clock'
 
 test('朝ならおはようございます', () => {
     const getcurrentTime = Clock.prototype.getCurrentTime = jest.fn();
     getcurrentTime.mockImplementationOnce(() => new Date('2000-01-01T08:00:00.000'))
-    const greeter = new Greeter(new Clock())
+    const greeter = new Greeter(new Clock(), new MorningTimeRange())
     expect(greeter.greet()).toBe('おはようございます')
 })
 
 test('朝でないなら挨拶しない', () => {
     const getcurrentTime = Clock.prototype.getCurrentTime = jest.fn();
     getcurrentTime.mockImplementationOnce(() => new Date('2000-01-01T22:00:00.000'))
-    const greeter = new Greeter(new Clock())
+    const greeter = new Greeter(new Clock(), new MorningTimeRange())
     expect(greeter.greet()).toBe('')
 })
