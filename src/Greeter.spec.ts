1ba09c1d9d7f2f4418f8f800c2e61f97ec87943f
kken0201 <kken.com@gmail.com>
2020-02-11 18:12:29 +0900

    test: MorningTimeRangeはモックする

--- src/Greeter.spec.ts
+++ src/Greeter.spec.ts
@@ -5,6 +5,8 @@ import Clock from './Clock'
 test('朝ならおはようございます', () => {
     const getcurrentTime = Clock.prototype.getCurrentTime = jest.fn();
     getcurrentTime.mockImplementationOnce(() => new Date('2000-01-01T08:00:00.000'))
+    const contains = MorningTimeRange.prototype.contains = jest.fn()
+    contains.mockImplementationOnce(() => true)
     const greeter = new Greeter(new Clock(), new MorningTimeRange())
     expect(greeter.greet()).toBe('おはようございます')
 })
@@ -12,6 +14,8 @@ test('朝ならおはようございます', () => {
 test('朝でないなら挨拶しない', () => {
     const getcurrentTime = Clock.prototype.getCurrentTime = jest.fn();
     getcurrentTime.mockImplementationOnce(() => new Date('2000-01-01T22:00:00.000'))
+    const contains = MorningTimeRange.prototype.contains = jest.fn()
+    contains.mockImplementationOnce(() => false)
     const greeter = new Greeter(new Clock(), new MorningTimeRange())
     expect(greeter.greet()).toBe('')
 })
