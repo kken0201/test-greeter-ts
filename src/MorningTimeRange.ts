484e38d92006b89eff08164ab6aca0b42b8f1ec9
kken0201 <kken.com@gmail.com>
2020-02-11 18:04:00 +0900

    MorningTimeRangeの実装を追加しテストをパス

--- src/MorningTimeRange.ts
+++ src/MorningTimeRange.ts
@@ -1,5 +1,8 @@
+import { getHours } from 'date-fns'
+
 export default class MorningTimeRange {
     contains(target: Date): boolean {
-        return true
+        const hour = getHours(target)
+        return hour >= 5 && hour < 12
     }
 }
